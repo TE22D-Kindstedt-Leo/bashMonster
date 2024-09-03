215331bb38bd58f1c0fe8f6d5a7e1c3c977462f0
TE22D-Kindstedt-Leo <leo.kindstedt@elev.ga.ntig.se>
2024-09-03 10:16:22 +0200

    kljmuijipu

---
+++ BashMonster/Program.cs
@@ -0,0 +1,16 @@
+﻿using System.Reflection.Metadata.Ecma335;
+
+karaktär theKaraktär = new karaktär();
+Fiende theFiende = new Fiende();
+
+
+Console.WriteLine($"{theKaraktär.namn} har {theKaraktär.hälsa} hälsa");
+Console.WriteLine($"{theFiende.namn} har {theFiende.hälsa} hälsa");
+
+Console.WriteLine("ATTACK!");
+theFiende.hälsa -= 10;
+
+Console.WriteLine($"{theFiende.namn} har {theFiende.hälsa} hälsa");
+
+Console.WriteLine("Press any key to continue...");
+Console.ReadKey();
---
+++ BashMonster/Skill.cs
@@ -0,0 +1,14 @@
+public class Fiende 
+{
+    public int hälsa = 150;
+    public int attack = 10;
+    public string namn = "Fiende";
+}
+
+public class karaktär
+{
+    public int hälsa = 100;
+    public int attack = 10;
+    public string namn = "Karaktär";
+}
+
